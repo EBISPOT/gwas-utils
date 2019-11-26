aaa358a693a2349d184db345361f5e9c0ba6a82b
Daniel Suveges <suvi.dani@gmail.com>
2019-11-26 16:00:26 +0000

    Adding module to manage LSF jobs.

--- solr_indexer_manager/components/lsf_manager.py
+++ solr_indexer_manager/components/lsf_manager.py
@@ -1,18 +1,118 @@
-import numpy as np
+import subprocess
+import os
+import re
+from collections import OrderedDict
 
-def call_generator(db_updates, wrapper):
-    jobs = 30   # Maximum number of jobs
-    pmids = [z for x in db_updates.values() for z in x ]  # List of all pubmed ID which will be submitted:
-
-    calls = []
-    for chunk in np.array_split(np.array(pmids),jobs):
-        calls.append('{} -d -e -p {}'.format(wrapper, ','.join(chunk)))
-
-    return calls
+class LSF_manager(object):
+    
+    jobs = {}
+    
+    submitCommand = 'bsub'
+    
+    def __init__(self, queue = None, memory = 2000, job_prefix = 'job', 
+                 workingDir = '.', notify = None, job_group = None):
         
+        # compile bsub parameters:
+        self.bsub_parameters = OrderedDict({
+                '-M' : str(memory),
+                '-R' : "select[mem>{}] rusage[mem={}]".format(memory, memory),
+                '-J' : job_prefix,
+                '-o' : job_prefix + '.o',
+                '-e' : job_prefix + '.e',
+        })
+            
+        # Add other variables:
+        if notify:
+            self.bsub_parameters['-N'] = notify
+            
+        if queue:
+            self.bsub_parameters['-q'] = queue
+            
+        if job_group:
+            self.bsub_parameters['-g'] = job_group
 
-def trait_calls(wrapper):
-    return [
-        '{} -a -s -e '.format(wrapper),
-        '{} -a -s -d '.format(wrapper)
-    ]
+        # Store other parameters:
+        self.workingDir = workingDir
+        self.job_prefix = job_prefix
+        
+    def submit_job(self, command, workingDir = None, jobname = None):
+        '''
+        This method accepts a job and compiles into a submittable bsub command. And tries to submit.
+        '''
+        
+        # Generate individual job description:
+        command_parameters = self.bsub_parameters.copy()
+        
+        # Update job name if provided:
+        if jobname:
+            command_parameters['-J'] = jobname
+            command_parameters['-o'] = jobname + '.o'
+            command_parameters['-e'] = jobname + '.e'
+        
+        # Compile bjob:
+        command_array = ['bsub']
+        [command_array.extend([k,v]) for k,v in command_parameters.items()]
+        command_array.append(command)
+        
+        # Update working directory:
+        if not workingDir:
+            workingDir = self.workingDir
+        
+        # Submit job to LSF:
+        x = Popen(command_array ,stdout=PIPE, stderr=PIPE, cwd=workingDir)
+
+        # Parse output:
+        output = x.communicate()
+        stdout = output[0]
+        stderr = output[1]
+
+        if stderr:
+            print('[Error] Submitting job failed: {}'.format(stderr))
+            return(1)
+
+        m = re.search('(\d+)', str(stdout))
+        jobID = m.group(0)
+        print('[Info] Job has been successfully submitted. Job ID: {}'.format(jobID))
+   
+        # Adding job to variable:
+        self.jobs.update({
+            jobID : {
+                'status' : 'submitted.'
+            }
+        })
+
+    def check_job(self,jobID):
+        x = Popen(['bjobs', '-a', jobID], stdout=PIPE, stderr=PIPE)
+        y = Popen(['tail', '-n+2'], stdin=x.stdout, stdout=PIPE)
+        x.stdout.close()
+        
+        output = y.communicate()
+        stdout = str(output[0])
+        
+        # If status is not available for the queried jobID:
+        if output[1]:
+            self.jobs[jobID]['status'] = None
+            return(None)
+        
+        # Parsing status if available:
+        else:
+            jobStatus = stdout.split(" ")[2]
+            self.jobs[jobID]['status'] = jobStatus
+
+
+    def generate_report(self):
+        self.update_job_status()
+        statuses = {}
+        # Looping through all jobs and getting the statuses:
+        for jobID in self.jobs.keys():
+            try:
+                statuses[self.jobs[jobID]['status']] += 1
+            except KeyError
+                statuses[self.jobs[jobID]['status']] = 1
+        return statuses
+        
+    def update_job_status(self):
+        for jobID in self.jobs.keys():
+            if self.jobs[jobID]['status'] == 'DONE':
+                continue
+
--- solr_indexer_manager/indexer_manager.py
+++ solr_indexer_manager/indexer_manager.py
@@ -2,11 +2,57 @@ import argparse
 import pandas as pd
 from datetime import date
 from solrWrapper import solrWrapper
+import time
 
 # Loading components:
 from components import wrapper_manager
 from components import getUpdated
 from components import solrUpdater
+from components import lsf_manager
+
+
+def manage_lsf_jobs(wrapper_calls, trait_calls, workingDir):
+    '''
+    This function handles the lsf jobs. Monitors their progression and decides when to exit and how.... boy it needs to be improved.
+    '''
+
+    # Hardcoded variables:
+    memoryLimit = 2000
+    jobPrefix = 'solr_indexing'
+    jobGroup = '/gwas_catalog/solr_indexer'
+
+    # Initialize lsf object:
+    LSF_obj = LSF_manager(memory=memoryLimit, job_prefix=jobPrefix, job_group=jobGroup, workingDir=workingDir)
+
+    # Looping though all jobs:
+    folder_index = 0
+    for job in wrapper_calls + trait_calls:
+        try:
+            os.mkdir('{}/{}'.format(workingDir, folder_index))
+        except FileExistsError:
+            print('[Warning] folder already exists: {}/{}'.format(logDir, i))
+            continue
+
+        # Submit job to farm:
+        LSF_obj.submit_job(job, workingDir='{}/{}'.format(workingDir, folder_index))
+
+    # Wait until all jobs are finished:
+    while True:
+        report = LSF_obj.generate_report()
+
+        print('[Info] The statuses of the submitted jobs are:')
+        for status, count in report.items():
+            print("\t{}: {}".format(status, count))
+
+        if 'RUN' not in report and 'PEND' not in report:
+            break
+
+        time.sleep(1800)
+
+    # Having this means all the jobs are finished:
+    report = LSF_obj.generate_report()
+    return report
+
 
 if __name__ == '__main__':
 
@@ -24,6 +70,9 @@ if __name__ == '__main__':
 
     # Input related to solr indexer:
     parser.add_argument('--wrapperScript', help='Wrapper script for the solr indexer.')
+
+    # Location for log files:
+    parser.add_argument('--logFolder', help='Folder into which the log files will be generated.')
     args = parser.parse_args()
 
     # Parser out database instance names:
@@ -37,6 +86,9 @@ if __name__ == '__main__':
 
     # Parse wrapper:
     wrapperScript = args.wrapperScript
+
+    # Parse log directory:
+    logDir = args.logFolder
 
     # Determine updates by comparing old and new database instances:
     old_table = getUpdated.get_studies(oldInstance)
@@ -59,4 +111,7 @@ if __name__ == '__main__':
 
     print('[Info] Trait chunks:')
     print('\n\t'.join(trait_calls))
+
+    # Submitting the jobs to the farm:
+    manage_lsf_jobs(wrapper_calls, trait_calls, logDir)
 
