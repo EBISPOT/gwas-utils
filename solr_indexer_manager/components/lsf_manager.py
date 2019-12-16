ee93065422d4495569007694d3eff022e4ea1ba5
Daniel Suveges <suvi.dani@gmail.com>
2019-12-16 15:33:35 +0000

    Make job managenemt smarter.

--- solr_indexer_manager/components/lsf_manager.py
+++ solr_indexer_manager/components/lsf_manager.py
@@ -4,16 +4,17 @@ from collections import OrderedDict
 
 class LSF_manager(object):
     
-    jobs = {}
+    jobs = []
     
     submitCommand = 'bsub'
     
-    def __init__(self, queue = None, memory = 2000, job_prefix = 'job', 
+    def __init__(self, queue = None, memory = 2000, cpus = 1, job_prefix = 'job', 
                  workingDir = '.', notify = None, job_group = None):
         
         # compile bsub parameters:
         self.bsub_parameters = OrderedDict({
                 '-M' : str(memory),
+                '-n' : cpus,
                 '-R' : "select[mem>{}] rusage[mem={}]".format(memory, memory),
                 '-J' : job_prefix,
                 '-o' : job_prefix + '.o',
@@ -66,7 +67,7 @@ class LSF_manager(object):
         stderr = output[1]
 
         if stderr:
-            print('[Error] Submitting job failed: {}'.format(stderr))
+            print('[Error] Failure when submitting job:\n {}'.format(stderr))
             return(1)
 
         m = re.search('(\d+)', str(stdout))
@@ -74,11 +75,13 @@ class LSF_manager(object):
         print('[Info] Job has been successfully submitted. Job ID: {}'.format(jobID))
    
         # Adding job to variable:
-        self.jobs.update({
-            jobID : {
-                'status' : 'submitted.'
-            }
-        })
+        self.jobs.append({
+                'job_id'   : jobID,
+                'status'   : 'submitted.',
+                'command'  : command,
+                'working_dir' : workingDir, 
+                'job_name' : jobname
+            })
 
     def check_job(self,jobID):
         x = Popen(['bjobs', '-a', jobID], stdout=PIPE, stderr=PIPE)
@@ -99,21 +102,61 @@ class LSF_manager(object):
             self.jobs[jobID]['status'] = jobStatus
 
 
-    def generate_report(self):
+    def check_all_jobs(self):
         self.update_job_status()
         statuses = {}
-        
-        # Looping through all jobs and getting the statuses:
-        for jobID in self.jobs.keys():
-            try:
-                statuses[self.jobs[jobID]['status']] += 1
-            except KeyError:
-                statuses[self.jobs[jobID]['status']] = 1
-        return statuses
+        indices_to_delete = []
+        jobIDs_to_resubmit = []
+        jobIDs_to_kill = []
 
-    def update_job_status(self):
-        for jobID in self.jobs.keys():
-            if self.jobs[jobID]['status'] == 'DONE':
+        ##
+        ## Looping through all jobs and update statuses:
+        ##
+        for index,job in enumerate(self.jobs):
+
+            # If a job was finished, we don't care:
+            if job['status'] == 'DONE':
+                statuses['DONE'] ++
                 continue
-            self.check_job(jobID)
+
+            # Check for and record status:
+            jobID = job['job_id']
+            new_status = self.check_job(jobID)
+            job['status'] = new_status
+            statuses[new_status] ++
+
+            # We kill and resubmit a job if the status is not DONE, RUN or PEND
+            if new_status not in ['DONE', 'RUN', 'PEND']:
+                indices_to_delete.append(index)
+                jobIDs_to_kill.append(jobID)
+                jobIDs_to_resubmit.append(jobID)
+                statuses[new_status] ++ 
+                print('[Warning] A job (id: {}) was found with {} status. The job is killed and re-submitted to the farm.'.format(jobID, new_status))
+
+            if new_status == 'DONE':
+                statuses['DONE'] ++
+            else:
+                statuses[new_status] ++
+        
+        ## 
+        ## Print out report:
+        ##
+        print('[Info] Current statuses of the submitted jobs: {}'.format(','.join(['{} - {}'.format(k,v) for k,v in statuses.items()])))
+
+        ##
+        ## Delete failed jobs form list, kill the process and re-submit them:
+        ##
+        if indices_to_delete:
+            self.jobs = [v for i,v in enumerate(self.jobs) if i not in frozenset(indices_to_delete)]
+
+        # kill jobs:
+        for jobID in jobIDs_to_kill:
+            # kill job:
+            self.kill_jobID(jobID)
+
+        # re-submit command:
+        for jobID in jobIDs_to_resubmit:
+            job = self.jobs[jobID]
+            self.submit_job(command = job['command'], workingDir = job['working_dir'], jobname = job['job_name'])
+
 
