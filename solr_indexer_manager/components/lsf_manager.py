28fcb0f5444dc6dafd2a4546dfca24522f0b7d31
jstewart-ebi <jstewart@ebi.ac.uk>
2020-01-13 10:08:43 +0000

    fix job submit syntax errors

--- solr_indexer_manager/components/lsf_manager.py
+++ solr_indexer_manager/components/lsf_manager.py
@@ -8,13 +8,13 @@ class LSF_manager(object):
     
     submitCommand = 'bsub'
     
-    def __init__(self, queue = None, memory = 2000, cpus = 1, job_prefix = 'job', 
+    def __init__(self, queue = None, memory = 2000, cpus = '1', job_prefix = 'job',
                  workingDir = '.', notify = None, job_group = None):
         
         # compile bsub parameters:
         self.bsub_parameters = OrderedDict({
                 '-M' : str(memory),
-                '-n' : cpus,
+                '-n' : str(cpus),
                 '-R' : "select[mem>{}] rusage[mem={}]".format(memory, memory),
                 '-J' : job_prefix,
                 '-o' : job_prefix + '.o',
@@ -116,27 +116,27 @@ class LSF_manager(object):
 
             # If a job was finished, we don't care:
             if job['status'] == 'DONE':
-                statuses['DONE'] ++
+                statuses['DONE']+=1
                 continue
 
             # Check for and record status:
             jobID = job['job_id']
             new_status = self.check_job(jobID)
             job['status'] = new_status
-            statuses[new_status] ++
+            statuses[new_status]+=1
 
             # We kill and resubmit a job if the status is not DONE, RUN or PEND
             if new_status not in ['DONE', 'RUN', 'PEND']:
                 indices_to_delete.append(index)
                 jobIDs_to_kill.append(jobID)
                 jobIDs_to_resubmit.append(jobID)
-                statuses[new_status] ++ 
+                statuses[new_status]+=1
                 print('[Warning] A job (id: {}) was found with {} status. The job is killed and re-submitted to the farm.'.format(jobID, new_status))
 
             if new_status == 'DONE':
-                statuses['DONE'] ++
+                statuses['DONE']+=1
             else:
-                statuses[new_status] ++
+                statuses[new_status]+=1
         
         ## 
         ## Print out report:
