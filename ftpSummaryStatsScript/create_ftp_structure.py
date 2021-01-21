cc0ad78883bcbc900ceffc4a86d82a5fd1592366
jdhayhurst <hayhurst.jd@gmail.com>
2021-01-21 09:15:36 +0000

    script to move files and create symlinks

--- ftpSummaryStatsScript/create_ftp_structure.py
+++ ftpSummaryStatsScript/create_ftp_structure.py
@@ -14,8 +14,11 @@ import os
 import glob
 import re
 import numpy as np
+import shutil
 
-ftp_base = "staging"
+
+#full path required
+ftp_base = ""
 range_size = 1000
 
 sumstats = glob.glob(os.path.join(ftp_base, '*GCST*'))
@@ -24,7 +27,7 @@ def get_gcst_range(gcst):
     number_part = int(gcst.split("GCST")[1])
     floor = int(np.fix(number_part / range_size) * range_size) + 1
     upper = floor + (range_size -1)
-    range_str = "GCST{f}-GCST{u}".format(f=str(floor), u=str(upper))
+    range_str = "GCST{f}-GCST{u}".format(f=str(floor).zfill(6), u=str(upper).zfill(6))
     return range_str
 
 
@@ -36,15 +39,11 @@ for f in sumstats:
     gcst = gcst_regex.group(0) if gcst_regex else None
     if gcst:
         print(gcst)
-        print(get_gcst_range(gcst))
+        gcst_range = get_gcst_range(gcst)
+        new_dir = os.path.join(ftp_base, gcst_range, gcst)
+        # move dir
+        shutil.move(f, new_dir)
+        # create symlink
+        os.symlink(new_dir, f, target_is_directory=True)
     else:
         print("Couldn't find a GCST in {}".format(basename))
-
-
-#for d in pub
-# get gcst_range
-
-# mv dir to gcst_range/gcst
-
-# create symlink where it was
-
