e48fcdf9f6e844f34ae8f908c019f57879ce97aa
jdhayhurst <hayhurst.jd@gmail.com>
2021-02-04 12:56:24 +0000

    add test parameter

--- ftpSummaryStatsScript/create_ftp_structure.py
+++ ftpSummaryStatsScript/create_ftp_structure.py
@@ -15,13 +15,11 @@ import glob
 import re
 import numpy as np
 import shutil
+import argparse
 
 
-#full path required
-ftp_base = ""
 range_size = 1000
 
-sumstats = glob.glob(os.path.join(ftp_base, '*GCST*'))
 
 def get_gcst_range(gcst):
     number_part = int(gcst.split("GCST")[1])
@@ -30,20 +28,61 @@ def get_gcst_range(gcst):
     range_str = "GCST{f}-GCST{u}".format(f=str(floor).zfill(6), u=str(upper).zfill(6))
     return range_str
 
+def get_study_entries(ftp_base):
+    dir_list = glob.glob(os.path.join(ftp_base, '*GCST*'))
+    filtered_list = [d for d in dir_list if "-GCST" not in d]
+    return filtered_list
+    
 
-# get gcst
-for f in sumstats:
-    gcst = None
-    basename = os.path.basename(f)
-    gcst_regex = re.search(r'GCST[0-9]+', basename)
-    gcst = gcst_regex.group(0) if gcst_regex else None
-    if gcst:
-        print(gcst)
-        gcst_range = get_gcst_range(gcst)
-        new_dir = os.path.join(ftp_base, gcst_range, gcst)
-        # move dir
-        shutil.move(f, new_dir)
-        # create symlink
-        os.symlink(new_dir, f, target_is_directory=True)
-    else:
-        print("Couldn't find a GCST in {}".format(basename))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type=str, help='Full path to ftp directory containing sumstats.')
+    parser.add_argument('--test', action='store_true', help='If test run is specified, nothing is actually done.')
+    args = parser.parse_args()
+    
+    ftp_base = args.dir
+    # get gcst
+    sumstats = get_study_entries(ftp_base)
+    for f in sumstats:
+        print("======== processing... ========")
+        print(f)
+        gcst = None
+        basename = os.path.basename(f)
+        gcst_regex = re.search(r'GCST[0-9]+', basename)
+        gcst = gcst_regex.group(0) if gcst_regex else None
+        if gcst:
+            print(gcst)
+            gcst_range = get_gcst_range(gcst)
+            new_dir = os.path.join(ftp_base, gcst_range, gcst)
+            # move dir
+            # f is symlink
+            if os.path.islink(f):
+                print("{} is a symlink. Ignoring it.".format(f))
+            # new_dir exists but f is pmid
+            elif os.path.exists(new_dir) and "_GCST" in os.path.basename(f):
+                print("New dir exists. Move {} --> {}".format(f, new_dir))
+                print("create symlink: {}".format(f))
+                if args.test is False:
+                    shutil.rmtree(new_dir)
+                    shutil.move(f, new_dir)
+                    # create symlink
+                    os.symlink(new_dir, f, target_is_directory=True)
+            # new_dir doesn't exist
+            elif not os.path.exists(new_dir):
+                print("New dir does not exist. Move {} --> {}".format(f, new_dir))
+                print("create symlink: {}".format(f))
+                if args.test is False:
+                    shutil.move(f, new_dir)
+                    # create symlink
+                    os.symlink(new_dir, f, target_is_directory=True)
+            # new_dir exists but f is not pmid
+            else:
+                print("{} already exists, removing {} and creating symlink in place".format(new_dir, f))
+                shutil.rmtree(f)
+                os.symlink(new_dir, f, target_is_directory=True)
+        else:
+            print("Couldn't find a GCST in {}".format(basename))
+
+
+if __name__ == '__main__':
+    main()
