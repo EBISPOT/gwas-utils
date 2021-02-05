d4f616235eb20b46bc5a3899cc90adaa898349e7
jdhayhurst <hayhurst.jd@gmail.com>
2021-02-05 09:26:49 +0000

    changing from absolute to relative symlinks

--- ftpSummaryStatsScript/create_ftp_structure.py
+++ ftpSummaryStatsScript/create_ftp_structure.py
@@ -28,8 +28,8 @@ def get_gcst_range(gcst):
     range_str = "GCST{f}-GCST{u}".format(f=str(floor).zfill(6), u=str(upper).zfill(6))
     return range_str
 
-def get_study_entries(ftp_base, pattern):
-    dir_list = glob.glob(os.path.join(ftp_base, pattern))
+def get_study_entries(pattern):
+    dir_list = glob.glob(pattern)
     filtered_list = [d for d in dir_list if "-GCST" not in d]
     return filtered_list
     
@@ -37,30 +37,32 @@ def get_study_entries(ftp_base, pattern):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, help='Full path to ftp directory containing sumstats.')
-    parser.add_argument('--pattern', type=str, help='Pattern for the glob matching', default='*GCST*')
+    parser.add_argument('--pattern', type=str, help='Pattern for the glob matching - put this in quotes', default='*GCST*')
     parser.add_argument('--test', action='store_true', help='If test run is specified, nothing is actually done.')
     args = parser.parse_args()
     
     ftp_base = args.dir
+
+    # change directory (symlinks will need to be relative, not absolute)
+    os.chdir(ftp_base)
     # get gcst
-    sumstats = get_study_entries(ftp_base, args.pattern)
+    sumstats = get_study_entries(args.pattern)
     for f in sumstats:
         print("======== processing... ========")
         print(f)
         gcst = None
-        basename = os.path.basename(f)
-        gcst_regex = re.search(r'GCST[0-9]+', basename)
+        gcst_regex = re.search(r'GCST[0-9]+', f)
         gcst = gcst_regex.group(0) if gcst_regex else None
         if gcst:
             print(gcst)
             gcst_range = get_gcst_range(gcst)
-            new_dir = os.path.join(ftp_base, gcst_range, gcst)
+            new_dir = os.path.join(gcst_range, gcst)
             # move dir
             # f is symlink
             if os.path.islink(f):
                 print("{} is a symlink. Ignoring it.".format(f))
             # new_dir exists but f is pmid
-            elif os.path.exists(new_dir) and "_GCST" in os.path.basename(f):
+            elif os.path.exists(new_dir) and "_GCST" in f:
                 print("New dir exists. Move {} --> {}".format(f, new_dir))
                 print("create symlink: {}".format(f))
                 if args.test is False:
@@ -83,7 +85,7 @@ def main():
                     shutil.rmtree(f)
                     os.symlink(new_dir, f, target_is_directory=True)
         else:
-            print("Couldn't find a GCST in {}".format(basename))
+            print("Couldn't find a GCST in {}".format(f))
 
 
 if __name__ == '__main__':
