2d4d3c84e5b5c7edc37a16ca034e8e590a3a5ffd
jdhayhurst <hayhurst.jd@gmail.com>
2022-06-01 18:00:07 +0100

    adding nextflow for managing solr indexing on lsf

--- solrIndexerManager/indexer_manager.py
+++ solrIndexerManager/indexer_manager.py
@@ -3,74 +3,154 @@ from datetime import datetime
 from solrWrapper import solr_wrapper
 import time
 import os
+import json
+import subprocess
 
 # Loading components:
 from solrIndexerManager.components import getUpdated
 from solrIndexerManager.components import solrUpdater
 from solrIndexerManager.components import lsf_manager
 
-def job_generator(db_updates, wrapper):
-    """
-    This function generates the jobs based on the provided wrapper script and the dictionary with the db updates.
+class IndexerManager:
+    def __init__(self, 
+                 newInstance=None, 
+                 oldInstance=None, 
+                 solrHost=None, 
+                 solrPort=None, 
+                 solrCore=None, 
+                 wrapperScript=None, 
+                 logDir=None, 
+                 fullIndex=None, 
+                 memory=None, 
+                 job_prefix=None, 
+                 job_group=None, 
+                 workingDir=None, 
+                 queue=None,
+                 nfScriptPath=None):
+        self.newInstance = newInstance
+        self.oldInstance = oldInstance
+        self.solrHost = solrHost
+        self.solrPort = solrPort
+        self.solrCore = solrCore
+        self.wrapperScript = wrapperScript
+        self.logDir = logDir
+        self.fullIndex = fullIndex
+        self.memory = memory
+        self.job_prefix = job_prefix
+        self.job_group = job_group
+        self.workingDir = workingDir
+        self.queue = queue
+        self.db_updates = None
+        self.nfScriptPath = nfScriptPath
+        self.job_file = None
+        
 
-    Return data:
-    {
-        ${pmid} : './${wrapper} -d -e -p ${pmid}',
-        ...
-        efotrait : './${wrapper} -a -s -d',
-        diseasetrait : './${wrapper} -a -s -e'
-    }
-    """
+    def job_generator(self):
+        """
+        This function generates the jobs based on the provided wrapper script and the dictionary with the db updates.
+    
+        Return data:
+        {
+            ${pmid} : './${wrapper} -d -e -p ${pmid}',
+            ...
+            efotrait : './${wrapper} -a -s -d',
+            diseasetrait : './${wrapper} -a -s -e'
+        }
+        """
+    
+        # Indexing jobs with associations and studies for each pubmed ID:
+        jobs = {pmid : '{} -d -e -p {}'.format(self.wrapperScript, pmid) for x in self.db_updates.values() for pmid in x if pmid != '*'}
+        # Indexing job to generate disease trait and efo trait documents:
+        jobs['efo_traits'] = '{} -a -s -d '.format(self.wrapperScript)
+        jobs['disease_traits'] = '{} -a -s -e '.format(self.wrapperScript)
+        return jobs
 
-    # Indexing jobs with associations and studies for each pubmed ID:
-    jobs = {pmid : '{} -d -e -p {}'.format(wrapper, pmid) for x in db_updates.values() for pmid in x if pmid != '*'}
 
-    # Indexing job to generate disease trait and efo trait documents:
-    jobs['efo_traits'] = '{} -a -s -d '.format(wrapper)
-    jobs['disease_traits'] = '{} -a -s -e '.format(wrapper)
+    def get_database_updates(self):
+        # Determine updates by comparing old and new database instances:
+        new_table = getUpdated.get_studies(self.newInstance)
+        # The update object is generated depending on if the flag is enabled or not:
+        if self.fullIndex:
+            db_updates = {
+                "added": new_table.PUBMED_ID.unique().tolist(),  # As if all publications in the new table was newly added
+                "removed": ['*'],  # As if all publications were removed.
+                "updated": []
+            }
+        else:
+            old_table = getUpdated.get_studies(self.oldInstance)
+            db_updates = getUpdated.get_db_updates(old_table, new_table)
+        return db_updates
+    
+    def set_database_updates(self, db_updates):
+        self.db_updates = db_updates
 
-    return jobs
+    def generate_job_list_file(self):
+        job_map = self.job_generator()
+        self.job_file = os.path.join(self.logDir, 'job_map.csv')
+        with open(self.job_file, 'w') as f:
+            for k, v in job_map.items():
+                s = ",".join([k, v]) + "\n"
+                f.write(s)
+                
+    def prepare_solr(self):
+        # Instantiate solr object:
+        solr_object = solr_wrapper.solrWrapper(host=self.solrHost, port=self.solrPort, core=self.solrCore)
+        # Removed associations and studies for all updated/deleted studies + removing all trait documents:
+        solrUpdater.removeUpdatedSolrData(solr_object, self.db_updates)
+        
+    def run_indexer(self):
+        nextflow_cmd = """
+                       nextflow {nf} --job_map_file {jm}
+                       """.format(nf=self.nfScriptPath, jm=self.job_file)
+        print("Running nextflow: {}".format(nextflow_cmd))
+        subproc_cmd = nextflow_cmd.split()
+        process = subprocess.run(subproc_cmd)
+        print(process.stdout)
 
-def manage_lsf_jobs(job_list, workingDir):
-    '''
-    This function handles the lsf jobs. Monitors their progression and decides when to exit and how.... boy it needs to be improved.
-    '''
 
-    # Hardcoded variables:
-    memoryLimit = 8000
-    jobPrefix = 'solr_indexing'
-    jobGroup = '/gwas_catalog/solr_indexer'
-    queue = 'production'
+#def manage_lsf_jobs(job_map, workingDir):
+#    '''
+#    This function handles the lsf jobs. Monitors their progression and decides when to exit and how.... boy it needs to be improved.
+#    '''
+#
+#    # Hardcoded variables:
+#    memoryLimit = 8000
+#    jobPrefix = 'solr_indexing'
+#    jobGroup = '/gwas_catalog/solr_indexer'
+#    queue = 'production'
+#
+#    # Initialize lsf object:
+#    LSF_obj = lsf_manager.LSF_manager(memory=memoryLimit, job_prefix=jobPrefix, job_group=jobGroup, workingDir=workingDir, queue=queue)
+#
+#    # Looping though all jobs:
+#    folder_index = 0
+#
+#    # Looping through all the jobs, create separate folders and submit each to the farm:
+#    for job_ID, job in job_list.items():
+#        try:
+#            os.mkdir('{}/{}'.format(workingDir, job_ID))
+#        except FileExistsError:
+#            print('[Warning] folder already exists: {}/{}'.format(workingDir, job_ID))
+#
+#        # Submit job to farm:
+#        LSF_obj.submit_job(job, workingDir='{}/{}'.format(workingDir, job_ID), jobname = job_ID)
+#
+#    # Wait until all jobs are finished or terminated:
+#    while True:
+#        report = LSF_obj.generate_report()
+#
+#        print('[Info] Checking statuses of the submitted jobs at: {:%b %d %H:%M}'.format(datetime.now()))
+#        for status, count in report.items():
+#            print("\t{}: {}".format(status, count))
+#
+#        if 'RUN' not in report and 'PEND' not in report and 'EXIT' not in report:
+#            print('[Info] No running or pending jobs were found. Exiting.')
+#            break
+#
+#        time.sleep(600)
 
-    # Initialize lsf object:
-    LSF_obj = lsf_manager.LSF_manager(memory=memoryLimit, job_prefix=jobPrefix, job_group=jobGroup, workingDir=workingDir, queue=queue)
 
-    # Looping though all jobs:
-    folder_index = 0
 
-    # Looping through all the jobs, create separate folders and submit each to the farm:
-    for job_ID, job in job_list.items():
-        try:
-            os.mkdir('{}/{}'.format(workingDir, job_ID))
-        except FileExistsError:
-            print('[Warning] folder already exists: {}/{}'.format(workingDir, job_ID))
-
-        # Submit job to farm:
-        LSF_obj.submit_job(job, workingDir='{}/{}'.format(workingDir, job_ID), jobname = job_ID)
-
-    # Wait until all jobs are finished or terminated:
-    while True:
-        report = LSF_obj.generate_report()
-
-        print('[Info] Checking statuses of the submitted jobs at: {:%b %d %H:%M}'.format(datetime.now()))
-        for status, count in report.items():
-            print("\t{}: {}".format(status, count))
-
-        if 'RUN' not in report and 'PEND' not in report and 'EXIT' not in report:
-            print('[Info] No running or pending jobs were found. Exiting.')
-            break
-
-        time.sleep(600)
 
 
 def main():
@@ -95,8 +175,8 @@ def main():
                         action="store_true")
 
     # Location for log files:
-    parser.add_argument('--logFolder', help='Folder into which the log files will be generated.')
-
+    parser.add_argument('--logFolder', help='Folder into which the log files will be generated.', default="./")
+    parser.add_argument('--nfScript', help='Nextflow script path', default="solrIndexerManager/solr_indexing.nf")
     # Print out excessive reports:
     parser.add_argument('--verbose', help='Flag to give more informative output.', action="store_true")
     args = parser.parse_args()
@@ -110,6 +190,7 @@ def main():
     solrCore = args.solrCore
     solrPort = args.solrPort
     fullIndex = args.fullIndex
+    nfScriptPath = args.nfScript
     verbose = args.verbose
 
     # Parse wrapper:
@@ -118,41 +199,20 @@ def main():
     # Parse log directory:
     logDir = args.logFolder
 
-    # Determine updates by comparing old and new database instances:
-    old_table = getUpdated.get_studies(oldInstance)
-    new_table = getUpdated.get_studies(newInstance)
-
-    # The update object is generated depending on if the flag is enabled or not:
-    if fullIndex:
-        db_updates = {
-            "added": new_table.PUBMED_ID.unique().tolist(),  # As if all publications in the new table was newly added
-            "removed": ['*'],  # As if all publications were removed.
-            "updated": []
-        }
-    else:
-        db_updates = getUpdated.get_db_updates(old_table, new_table)
-
-    # Instantiate solr object:
-    solr_object = solr_wrapper.solrWrapper(host=solrHost, port=solrPort, core=solrCore, verbose=True)
-
-    # Removed associations and studies for all updated/deleted studies + removing all trait documents:
-    solrUpdater.removeUpdatedSolrData(solr_object, db_updates)
-
-    # Generate a list of jobs:
-    joblist = job_generator(db_updates, wrapperScript)
-
-    # Print reports before submit to farm:
-    if verbose:
-        print('[Info] List of jobs to be submitted to the farm:')
-        print('\n\t'.join(joblist.values()))
-
-    # Submitting the jobs to the farm:
-    manage_lsf_jobs(joblist, logDir)
-
-    # At this point there's no downstream management of the jobs.... just submit and wait.
-    # we are assuming things went well. If not downstream QC measures will terminate the plan anyway.
-    # Later time based on experience this can be made more sophisticated.
-
+    manager = IndexerManager(newInstance=newInstance, 
+                             oldInstance=oldInstance, 
+                             solrHost=solrHost, 
+                             solrPort=solrPort,
+                             solrCore=solrCore, 
+                             wrapperScript=wrapperScript,
+                             logDir=logDir, 
+                             fullIndex=fullIndex,
+                             nfScriptPath=nfScriptPath)
+    db_updates = manager.get_database_updates()
+    manager.set_database_updates(db_updates=db_updates)
+    manager.generate_job_list_file()
+    manager.prepare_solr()
+    manager.run_indexer()
 
 if __name__ == '__main__':
     main()
--- solrIndexerManager/solr_indexing.nf
+++ solrIndexerManager/solr_indexing.nf
@@ -1,99 +1,34 @@
-params.newInstance = ''
-params.oldInstance = ''
-params.solrHost = ''
-params.solrCore = ''
-params.solrPort = ''
-params.wrapperScript = ''
-params.fullIndex = 'True'
+nextflow.enable.dsl=2
+
+params.job_map_file = ''
+params.log_path = ''
 
 // ------------------------------------------------------------- //
-// Determine updates from databases and update Solr              //
+// Run the solr indexer jobs                                     //
 // ------------------------------------------------------------- //
 
-process get_updates {
 
-  memory { 1.GB * task.attempt }
-  time { 1.hour * task.attempt }
-  maxRetries 3
-  errorStrategy { task.exitStatus in 2..140 ? 'retry' : 'terminate' }
-
-  output:
-  file 'db_updates.json' into db_updates
-
-  """
-  #!/usr/bin/env python
-
-  import json
-  from solrIndexerManager.components import getUpdated
-  from solrWrapper import solr_wrapper
-  from solrIndexerManager.components import solrUpdater
-
-  # Get updates
-  new_table = getUpdated.get_studies("$params.newInstance")
-  if $params.fullIndex:
-      db_updates = {
-          "added": new_table.PUBMED_ID.unique().tolist(),  # As if all publications in the new table was newly added
-          "removed": ['*'],  # As if all publications were removed.
-          "updated": []
-      }
-  else:
-      old_table = getUpdated.get_studies("$params.oldInstance")
-      db_updates = getUpdated.get_db_updates(old_table, new_table)
-
-  # Removed associations and studies for all updated/deleted studies + removing all trait documents:
-  solr_object = solr_wrapper.solrWrapper(host="$params.solrHost", port="$params.solrPort", core="$params.solrCore", verbose=True)
-  solrUpdater.removeUpdatedSolrData(solr_object, db_updates)
-
-  with open('db_updates.json', 'w') as f:
-      json.dump(db_updates, f)
-  """
-}
-
-
-// ------------------------------------------------------------- //
-// Run the solr indexer                                          //
-// ------------------------------------------------------------- //
-
-process solr_indexer {
-
+process run_solr_indexer {
+  tag "$id"
   memory { 2.GB * task.attempt }
   time { 4.hour * task.attempt }
   maxRetries 3
   errorStrategy { task.exitStatus in 2..140 ? 'retry' : 'terminate' }
 
   input:
-  file db_updates_file from db_updates
+  tuple val(id), val(cmd)
 
   output:
-  stdout into ch
+  stdout
 
   """
-  #!/usr/bin/env python
-
-  import json
-
-
-  db_updates = None
-  with open("${db_updates_file}") as f:
-      db_updates = json.load(f)
-
-  def job_generator(db_updates, wrapper):
-      # Indexing jobs with associations and studies for each pubmed ID:
-      jobs = {pmid : '{} -d -e -p {}'.format(wrapper, pmid) for x in db_updates.values() for pmid in x if pmid != '*'}
-      # Indexing job to generate disease trait and efo trait documents:
-      jobs['efo_traits'] = '{} -a -s -d '.format(wrapper)
-      jobs['disease_traits'] = '{} -a -s -e '.format(wrapper)
-      return jobs
-
-  joblist = job_generator(db_updates, "$params.wrapperScript")
-  print(joblist)
+  echo $id
+  $cmd
   """
 }
 
 
-
-
-ch.view { it }
-
-
-
+workflow {
+  jobs = channel.fromPath("$params.job_map_file").splitCsv()
+  run_solr_indexer(jobs)
+}
