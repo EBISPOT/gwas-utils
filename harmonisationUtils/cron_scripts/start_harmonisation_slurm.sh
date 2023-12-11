f9fd78fd9695841d83319c4d1ffbc958eb1f2c7d
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2023-12-11 17:20:52 +0000

    feat: Add LSF scripts and SLURM versions, remove *.sh from gitignore
    - Added existing crontab scripts utilizing LSF
    - Introduced SLURM script versions
    - Removed *.sh pattern from .gitignore to track shell scripts

---
+++ harmonisationUtils/cron_scripts/start_harmonisation_slurm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+day=$(date -d "yesterday 13:00" "+%Y%m%d")
+
+logs="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/logs/${day}"
+
+# DEV ENV
+#wrapper="/hps/software/users/parkinso/spot/gwas/dev/gwas-utils/harmonisationUtils/harmonisation_wrapper.sh"
+#version="nextflow"
+#pub_ftp="/nfs/ftp/private/gwas_cat/harm_test/success"
+
+wrapper="/hps/software/users/parkinso/spot/gwas/prod/scripts/gwas-utils/harmonisationUtils/harmonisation_wrapper.sh"
+ref="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/resources"
+to_harm="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/toharm/${day}"
+pub_ftp="/nfs/ftp/public/databases/gwas/summary_statistics"
+failed="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/failed/${day}"
+version="v1.1.3"
+mail_add="gwas-dev-logs@ebi.ac.uk"
+file_type="gwas_ssf"
+nf_conf="/hps/software/users/parkinso/spot/gwas/prod/scripts/cron/container.config"
+
+cmd="${wrapper} ${ref} ${to_harm} ${pub_ftp} ${failed} ${version} ${logs} ${mail_add} ${file_type} ${nf_conf}"
+
+if [[ -d "$to_harm"  ]]; then
+	mkdir -p $logs
+	sbatch --output="${logs}/nf.out" --error="${logs}/nf.err" --mem=8000 --time=02:00:00 --job-name=gwas_ss_harmo --wrap="${cmd}"
+else
+	echo "Nothing to harmonise today"
+fi
