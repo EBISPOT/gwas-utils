4c02c9597fd68d8c5fd8ec9b52b9fbfe10566ef0
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2024-03-19 10:18:40 +0000

    chore: Update release versions (#167)

--- harmonisationUtils/cron_scripts/start_harmonisation_pre_standard_slurm.sh
+++ harmonisationUtils/cron_scripts/start_harmonisation_pre_standard_slurm.sh
@@ -9,10 +9,10 @@ ref="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/resources"
 to_harm="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/pre_gwas_ssf/toharm/${day}"
 pub_ftp="/nfs/ftp/public/databases/gwas/summary_statistics"
 failed="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/pre_gwas_ssf/failed/${day}"
-version="v1.0.4"
+version="v1.0.5"
 mail_add="gwas-dev-logs@ebi.ac.uk"
 file_type="pre_gwas_ssf"
-nf_conf="/hps/software/users/parkinso/spot/gwas/prod/scripts/cron/container_pre_ssf.config"
+nf_conf="/hps/software/users/parkinso/spot/gwas/prod/scripts/cron/container_pre_ssf_slurm.config"
 
 cmd="${wrapper} ${ref} ${to_harm} ${pub_ftp} ${failed} ${version} ${logs} ${mail_add} ${file_type} ${nf_conf}"
 
--- harmonisationUtils/cron_scripts/start_harmonisation_slurm.sh
+++ harmonisationUtils/cron_scripts/start_harmonisation_slurm.sh
@@ -14,10 +14,10 @@ ref="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/resources"
 to_harm="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/toharm/${day}"
 pub_ftp="/nfs/ftp/public/databases/gwas/summary_statistics"
 failed="/hps/nobackup/parkinso/spot/gwas/data/sumstats/harmonisation/failed/${day}"
-version="v1.1.3"
+version="v1.1.4"
 mail_add="gwas-dev-logs@ebi.ac.uk"
 file_type="gwas_ssf"
-nf_conf="/hps/software/users/parkinso/spot/gwas/prod/scripts/cron/container.config"
+nf_conf="/hps/software/users/parkinso/spot/gwas/prod/scripts/cron/container_slurm.config"
 
 cmd="${wrapper} ${ref} ${to_harm} ${pub_ftp} ${failed} ${version} ${logs} ${mail_add} ${file_type} ${nf_conf}"
 
