3d7bbe3d856e88fb6df3f39fd890e28354ae409e
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2024-08-01 17:11:58 +0100

    Revert "test: Fail nf cmd on purpose"
    This reverts commit 954b36e3377425f3ccd6d42c6b9b5fc81a0bcd08.

--- solrIndexerManager/indexer_manager.py
+++ solrIndexerManager/indexer_manager.py
@@ -102,14 +102,13 @@ class IndexerManager:
         """
         Indexing is managed by a Nextflow workflow
         """
-        # nextflow_cmd = """
-        #                nextflow -log {logs} \
-        #                run {nf} \
-        #                --job_map_file {jm}
-        #                """.format(logs=os.path.join(self.logDir, "nextflow.log"), 
-        #                           nf=self.nfScriptPath, 
-        #                           jm=self.job_file)
-        nextflow_cmd = """false"""
+        nextflow_cmd = """
+                       nextflow -log {logs} \
+                       run {nf} \
+                       --job_map_file {jm}
+                       """.format(logs=os.path.join(self.logDir, "nextflow.log"), 
+                                  nf=self.nfScriptPath, 
+                                  jm=self.job_file)
         print("Running nextflow: {}".format(nextflow_cmd))
         subproc_cmd = nextflow_cmd.split()
 
@@ -118,14 +117,13 @@ class IndexerManager:
             print(process.stdout)
         except Exception as e:
             print(f"First attempt failed with error: {e}. Retrying with -resume option.")
-            # nextflow_cmd_resume = """
-            #                       nextflow -log {logs} \
-            #                       run {nf} \
-            #                       --job_map_file {jm} -resume
-            #                       """.format(logs=os.path.join(self.logDir, "nextflow.log"), 
-            #                           nf=self.nfScriptPath, 
-            #                           jm=self.job_file)
-            nextflow_cmd_resume = """nextflow --version"""
+            nextflow_cmd = """
+                           nextflow -log {logs} \
+                           run {nf} \
+                           --job_map_file {jm} -resume
+                           """.format(logs=os.path.join(self.logDir, "nextflow.log"), 
+                                      nf=self.nfScriptPath, 
+                                      jm=self.job_file)
             print("Running nextflow with -resume: {}".format(nextflow_cmd_resume))
             subproc_cmd_resume = nextflow_cmd_resume.split()
             
